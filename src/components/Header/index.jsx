1eb9d406229b6cbc91e18786c253c83123757015
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-04-08 21:22:06 -0300

    version_v2

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
 import styles from "./Header.module.css";
+import Image from "next/image";
+import logo from "/public/logo.png";
 
 export default function Header() {
   return (
     <header className={styles.container}>
-      <h1>Início</h1>
+      <Image
+        src={logo}
+        alt="Logo da empresa"
+        width={100}
+        height={100}
+        className={styles.logo}
+      />
+      <i className={`fas fa-magnifying-glass ${styles.maglass}`}></i>
+      <i className={`fa-solid fa-cart-shopping ${styles.cart}`}></i>
     </header>
   );
 }
