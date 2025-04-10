4a69ca7a3fdf59a7dda0cc4057e39ac9a6cac09e
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-04-10 00:04:03 -0300

    update_3
    Adição de imagens para capa e atualizações importantes no componente Cover

--- src/components/Cover/index.jsx
+++ src/components/Cover/index.jsx
@@ -1,16 +1,46 @@
 import styles from "./Cover.module.css";
 import Image from "next/image";
-import cover from "/public/cover.png";
+import { useEffect, useState } from "react";
+
+const images = ["/cover2.jpg", "/cover3.jpg", "/cover4.jpg"];
 
 export default function Cover() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [intervalTime, setIntervalTime] = useState(7000);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, intervalTime);
+
+    return () => clearInterval(interval);
+  }, [intervalTime, currentIndex]);
+
   return (
-    <div className={styles.main}>
-      <Image src={cover} alt="Capa" className={styles.cover} fill priority />
-      <div className={styles.text}>
-        <p>Preços baixos em</p>
-        <h1>ÓCULOS DE GRAU E DE SOL</h1>
-        <p>Você só encontra aqui</p>
-      </div>
+    <div className={styles.carousel}>
+      {images.map((img, index) => (
+        <div
+          key={index}
+          className={`${styles.slide} ${
+            index === currentIndex ? styles.active : ""
+          }`}
+        >
+          <Image
+            src={img}
+            alt={`Slide ${index}`}
+            layout="fill"
+            className={styles.image}
+            priority={index === 0}
+          />
+          {index === currentIndex && (
+            <div className={styles.textOverlay}>
+              <p>Preços baixos em</p>
+              <h1>ÓCULOS DE GRAU E DE SOL</h1>
+              <p>Você só encontra aqui</p>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
