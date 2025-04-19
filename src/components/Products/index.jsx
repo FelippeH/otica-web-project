a88e76edfa6984315f1cd5a89161a962ee382796
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-04-19 05:12:25 -0300

    update

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -9,70 +9,77 @@ const pGlasses = [
     discount: "27%",
     price: "R$399,00",
     save: "R$289,00",
-    payment: "10 x R$39,90",
+    payment: "10 x R$28,90",
     image: "/glasses01.png",
     hoverImage: "/glasses01_1.png",
   },
   {
     id: "óculos2",
     title: "1261 - Big Round F2 Cinza",
+    discount: "17%",
     price: "R$279,00",
     save: "R$229,00",
-    payment: "10 x R$27,90",
+    payment: "10 x R$22,90",
     image: "/glasses02.png",
     hoverImage: "/glasses02_1.png",
   },
   {
     id: "óculos3",
     title: "1984 - Medium Square T5 Black",
+    discount: "15%",
     price: "R$189,00",
     save: "R$159,00",
-    payment: "10 x R$18,90",
+    payment: "10 x R$15,90",
     image: "/glasses03.png",
     hoverImage: "/glasses03_1.png",
   },
   {
     id: "óculos4",
     title: "1112 - Medium Round G1 Cinza",
+    discount: "34%",
     price: "R$349,00",
     save: "R$229,00",
-    payment: "10 x R$34,90",
+    payment: "10 x R$22,90",
     image: "/glasses04.png",
     hoverImage: "/glasses04_1.png",
   },
   {
     id: "óculos5",
     title: "1691 - Large Square E4 Cinza",
+    discount: "12%",
     price: "R$549,00",
     save: "R$479,00",
-    payment: "10 x R$54,90",
+    payment: "10 x R$47,90",
     image: "/glasses05.png",
     hoverImage: "/glasses05_1.png",
   },
   {
     id: "óculos6",
     title: "1457 - Small Square H9 Black",
+    discount: "15%",
     price: "R$199,00",
     save: "R$169,00",
-    payment: "10 x R$19,90",
+    payment: "10 x R$16,90",
     image: "/glasses06.png",
     hoverImage: "/glasses06_1.png",
   },
   {
     id: "óculos7",
     title: "1292 - Small Square H5 Vermelho",
+    discount: "13%",
     price: "R$229,00",
     save: "R$199,00",
-    payment: "10 x R$22,90",
+    payment: "10 x R$19,90",
     image: "/glasses07.png",
     hoverImage: "/glasses07_1.png",
   },
   {
     id: "óculos8",
     title: "1552 - Large Round U2 Cinza",
+    discount: "35%",
     price: "R$599,00",
-    save: "R$509,00",
-    payment: "10 x R$59,90",
+    save: "R$389,00",
+    payment: "10 x R$38,90",
     image: "/glasses08.png",
     hoverImage: "/glasses08_1.png",
   },
@@ -174,7 +181,7 @@ export default function Products() {
                 fill
                 className={styles.image}
               />
-              <div className={styles.discountBadge}>-20%</div>
+              <div className={styles.discountBadge}>-{product.discount}</div>
               {product.hoverImage && (
                 <Image
                   src={product.hoverImage}
@@ -196,10 +203,9 @@ export default function Products() {
         <Image
           src="/promo01.jpg"
           alt="Promo Banner"
-          layout="responsive"
-          width={1920}
-          height={500}
+          fill
           quality={100}
+          style={{ objectFit: "cover" }}
         />
       </div>
       {/* Grid do catálogo de óculos de sol */}
