4273f54afd8ddfec9630b203a224baea0e30633a
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-05-04 12:39:55 -0300

    update
    criação do componente HomeContent

---
+++ src/components/HomeContent/index.jsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Cover from "@/components/Cover";
+import Products from "@/components/Products";
+import Newsletter from "@/components/Newsletter";
+import Footer from "@/components/Footer";
+
+export default function HomeContent() {
+  return (
+    <>
+      <Header />
+      <Cover />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
