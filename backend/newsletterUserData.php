19e059eb42db13d6777cb8457baa1abf60002c38
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-06-17 20:49:46 -0300

    add
    php config

---
+++ backend/newsletterUserData.php
@@ -0,0 +1,39 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
+include 'config.php';
+
+$name = $_POST['nome'] ?? '';
+$email = $_POST['email'] ?? '';
+
+if (empty($name) || empty($email)) {
+    echo "Nome e email são obrigatórios!";
+    exit;
+}
+
+try {
+$conn = new PDO("mysql:host=localhost;dbname=newsletter_aurora", "root", "");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stmt = $conn->prepare("INSERT INTO users (nome, email) VALUES (:nome, :email)");
+
+$stmt->bindParam(':nome', $name);
+$stmt->bindParam(':email', $email);
+
+if ($stmt->execute()) {
+    echo json_encode(["status" => "ok", "mensagem" => "Cadastro feito com sucesso!"]);
+} else {
+    echo json_encode(["status" => "erro", "mensagem" => "Erro ao inserir: " . $stmt->errorInfo()[2] 
+]);
+}
+
+} catch (PDOExcepction $e) {
+    echo "Erro: " . $e->getMessage();
+}
+?>
---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUser = 'root';
+$dbPass = '';
+$dbName = 'newsletter_aurora';
+
+try {
+    $conn = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+?>
