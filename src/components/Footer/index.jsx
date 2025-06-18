823723e814f7f6886dfe4b512cc4dc816413421e
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-06-18 16:18:22 -0300

    feat(ui): add styled line

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,8 +2,11 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <main className={styles.mainContainer}>
-      <p className={styles.text}>&copy; Desenvolvido por Felipe Stefano</p>
-    </main>
+    <>
+      <hr className={styles.hr} />
+      <div className={styles.mainContainer}>
+        <p className={styles.text}>&copy; Desenvolvido por Felipe Stefano</p>
+      </div>
+    </>
   );
 }
---
+++ src/components/FooterMenu/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./FooterMenu.module.css";
+
+export default function Footer() {
+  return (
+    <>
+      <div className={styles.main}>
+        <h1>test</h1>
+      </div>
+    </>
+  );
+}
