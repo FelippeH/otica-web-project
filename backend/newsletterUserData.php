40e33092a2c9fed90ba7dcdb660a31edf34facc6
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-06-19 14:29:37 -0300

    fix: syntax correction

--- backend/newsletterUserData.php
+++ backend/newsletterUserData.php
@@ -1,7 +1,9 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:3000/");
+
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
@@ -33,7 +35,7 @@ if ($stmt->execute()) {
 ]);
 }
 
-} catch (PDOExcepction $e) {
+} catch (PDOException $e) {
     echo "Erro: " . $e->getMessage();
 }
 ?>
